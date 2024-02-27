f30e9bcbed91241cbed3041ab41adbc73607408f
MinaAlmasi <mina.almasi00@gmail.com>
2024-02-27 13:37:09 +0100

    reverse right hand and left hand + add more docstring

--- src/agents.R
+++ src/agents.R
@@ -10,11 +10,12 @@ sigmoid <- function(x, tau) {
 }
 
 #' REINFORCEMENT_Agent
-#' previous_choice: vector of previous choice ( c(1,0) or c(0,1) )
-#' previous_values: vector of previous values as (right hand, left hand) e.g., c(0.8, 0.2)
-#' feedback: 1 if previous choice was correct, 0 if previous choice was incorrect
-#' alpha: learning rate
-#' tau: temperature parameter (controls randomness of choice). Tau at 0 -> stochastic
+#' Define a reinforcement learning agent
+#' - previous_choice: vector of previous choice e.g., c(1,0) meaning that right hand was chosen or c(0,1) meaning that the left hand was chosen
+#' - previous_values: vector of previous values as (right hand, left hand) e.g., c(0.8, 0.2)
+#' - feedback: 1 if previous choice was correct, 0 if previous choice was incorrect
+#' - alpha: learning rate
+#' - tau: temperature parameter (controls randomness of choice). Tau at 0 -> stochastic
 REINFORCEMENT_Agent <- function(previous_choice, previous_values, feedback, alpha, tau){
     
     # compute previous correct choice from feeddback and previous choice (if feedback is 1, previous correct choice is previous choice, otherwise it is the opposite)
@@ -24,12 +25,16 @@ REINFORCEMENT_Agent <- function(previous_choice, previous_values, feedback, alph
     # update value
     new_values = (1-alpha) * previous_values + alpha * previous_correct_choice
 
-    # convert to propability
-    val_diff = new_values[2] - new_values[1]
+    # convert difference in values (right minus left) to propability (of choosing right)
+    val_diff = new_values[1] - new_values[2]
     p = sigmoid(val_diff, tau)
 
     # make choice based on value
     choice = rbinom(1, 1, p)
+
+    # vectorize choice (if choice == 1 then the vector should )
+    if (choice == 1) {choice = c(1,0)}
+    else {choice = c(0,1)}
 
     # return choice and value
     return(list(choice, new_values))
