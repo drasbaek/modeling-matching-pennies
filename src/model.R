bdc9d1538f13a95e83540996e3dc69964114114b
MinaAlmasi <mina.almasi00@gmail.com>
2024-02-28 15:47:29 +0100

    working mdl, print samples

--- src/model.R
+++ src/model.R
@@ -11,22 +11,25 @@ df <- read_csv(df_filepath)
 dfs <- split(df, f = df$agent_id)
 
 # iterate over dfs
-for (i in 1:length(dfs)){
+for (i in 1:length(length(dfs))){
     # get the data correct
     df <- dfs[[i]]
-    data <- list("trials" = length(df$trial)/100, "choice"= df$choices, "feedback" = df$feedback)
+    data <- list("trials" = length(df$trial), "choice"= df$choices, "feedback" = df$feedback)
 
     # compile the model
     model <- cmdstan_model(stan_filepath, cpp_options = list(stan_threads = TRUE))
     
     # fit the model
-    fit <- model$sample(
+    samples <- model$sample(
         data = data,
         seed = 420,
+        threads_per_chain = 4,
         iter_warmup = 1000,
         iter_sampling = 2000,
         refresh = 500,
         max_treedepth = 10,
-        adopt_delta = 0.99
+        adapt_delta = 0.99
     )
     }
+
+print(samples$summary())
