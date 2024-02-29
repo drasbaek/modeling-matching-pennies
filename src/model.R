6330554f2e145f0dc7c98d4d2bf41e7e2c1789bb
daniblu <mr.daniblu@gmail.com>
2024-02-29 22:35:55 +0100

    adapt to working with lists

--- src/model.R
+++ src/model.R
@@ -5,6 +5,13 @@ stan_filepath = here::here("stan", "RL.stan")
 
 # fitting function
 fit_model <- function(data, onlyprior = 1){
+    
+    # prepare the data
+    data <- list("trials" = length(df$trial), 
+                 "choice"= df$choices, 
+                 "feedback" = df$feedback, 
+                 "onlyprior" = onlyprior)
+    
     # compile the model
     model <- cmdstan_model(stan_filepath, cpp_options = list(stan_threads = TRUE))
     
@@ -31,41 +38,36 @@ df <- read_csv(df_filepath)
 # split the df based on agent_id
 dfs <- split(df, f = df$agent_id)
 
-
 # sampling based on priors only
-prior_samples <- lapply(dfs, function(df){
-    # get the data correct
-    data <- list("trials" = length(df$trial), 
-                 "choice"= df$choices, 
-                 "feedback" = df$feedback, 
-                 "onlyprior" = 1)
-    
-    # fit the model
+prior_samples <- lapply(dfs, function(df){ # a list of samples objects, one per agent (n_games in total) 
     fit_model(data, onlyprior = 1)
 })
 
 # sampling based on priors and likelihood
 posterior_samples <- lapply(dfs, function(df){
-    # get the data correct
-    data <- list("trials" = length(df$trial), 
-                 "choice"= df$choices, 
-                 "feedback" = df$feedback, 
-                 "onlyprior" = 0)
-    
-    # fit the model
     fit_model(data, onlyprior = 0)
 })
 
 # extract choice predictions
-prior_predict_samples <- extract(prior_samples)$choice_pred # dim: 8000 samples x n_trials
-posterior_predict_samples <- extract(posterior_samples)$choice_pred
+prior_predict_samples <- lapply(prior_samples, function(samples){
+    extract(samples)$choice_pred # dim: 8000 samples x n_trials
+})
+
+posterior_predict_samples <- lapply(posterior_samples, function(samples){
+    extract(samples)$choice_pred
+})
 
 # calculate proportion of right-hand choices (rowwise proportion of 1s)
-prior_predict_right <- rowMeans(prior_predict_samples)
-posterior_predict_right <- rowMeans(posterior_predict_samples)
+prior_predict_right <- lapply(prior_predict_samples, function(samples){
+    rowMeans(samples)
+})
 
-# prior predictive check plot
-prior_predictive_check <- prior_predict_right %>%
+posterior_predict_right <- lapply(posterior_predict_samples, function(samples){
+    rowMeans(samples)
+})
+
+# prior predictive check plot from the first agent
+prior_predictive_check <- prior_predict_right[[1]] %>%
     as_tibble() %>%
     ggplot(aes(x = value)) +
     geom_histogram(binwidth = 0.01) +
@@ -75,8 +77,8 @@ prior_predictive_check <- prior_predict_right %>%
 
 ggsave(here::here("plots", "prior_predictive_check.png"), plot = prior_predictive_check)
 
-# posterior predictive check plot
-posterior_predictive_check <- posterior_predict_right %>%
+# posterior predictive check plot from the first agent
+posterior_predictive_check <- posterior_predict_right[[1]] %>%
     as_tibble() %>%
     ggplot(aes(x = value)) +
     geom_histogram(binwidth = 0.01) +
