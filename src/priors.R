869f249b519136148eadacd46c342f4ca19fb412
daniblu <mr.daniblu@gmail.com>
2024-03-02 23:03:58 +0100

    visualising the lower and upper bounds of value

--- src/priors.R
+++ src/priors.R
@@ -15,3 +15,14 @@ for (tau in tau_test) {
   }
   plot(x_values, y_values, type="l", main=paste("Sigmoid curve with tau=", tau), xlab="x", ylab="y")
 }
+
+# determining the space of value (set feedback to either 0 or 1)
+n_trials <- 20
+values <- c(0.5)
+alpha <- 0.3
+feedback <- 0
+for (trial in 2:n_trials) {
+  value <- values[trial-1] + alpha * (feedback - values[trial-1])
+  values <- c(values, value)
+}
+plot(1:n_trials, values, type="l", main="Learning curve", xlab="Trial", ylab="Value")
