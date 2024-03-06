1a6fae0f2f5390b6a2d2932b1f8bd905d1b03443
MinaAlmasi <mina.almasi00@gmail.com>
2024-03-06 16:22:46 +0100

    parameter recovery script + initial plots

--- src/parameter_recovery.R
+++ src/parameter_recovery.R
@@ -1,19 +1,21 @@
 # model fitting 
-pacman::p_load(tidyverse, here, cmdstanr)
+pacman::p_load(tidyverse, here, cmdstanr, pbapply)
 
 stan_filepath = here::here("stan", "RL.stan")
 
-# fitting function
-fit_model <- function(df, onlyprior = 1){
-    
+# 'fit_model' function
+# 'df' is the dataframe with the data
+# 'stan_filepath' is the path to the stan model
+# 'onlyprior' is a flag to fit the model with only the prior (1) or with the prior and likelihood (0)
+fit_model <- function(df, stan_filepath, onlyprior = 0){
     # prepare the data
     data <- list("trials" = length(df$trial), 
                  "choice"= df$choices, 
-                 "feedback" = df$feedback, 
+                 "feedback" = df$feedback, #ifelse(df$feedback == 0, -1, 1), 
                  "onlyprior" = onlyprior)
     
     # compile the model
-    model <- cmdstan_model(stan_filepath, cpp_options = list(stan_threads = TRUE))
+    model <- cmdstan_model(stan_filepath, cpp_options = list(stan_threads = TRUE), verbose = FALSE)
     
     # fit the model
     samples <- model$sample(
@@ -30,60 +32,62 @@ fit_model <- function(df, onlyprior = 1){
     return(samples)
 }
 
-# read data
-n_trials = 120
+MPD <- function(x) {
+  density(x)$x[which(density(x)$y==max(density(x)$y))]
+}
+
+n_trials <- 120
 df_filepath = here::here("data", paste0(n_trials, "_trials.csv"))
 df <- read_csv(df_filepath)
-
-# split the df based on agent_id
 dfs <- split(df, f = df$agent_id)
 
-# sampling based on priors only
-prior_samples <- lapply(dfs, function(df){ # a list of samples objects, one per agent (n_games in total) 
-    fit_model(data, onlyprior = 1)
+# fit models
+samples_list = pblapply(dfs, function(df) { # progress bar list apply
+    fit_model(df, stan_filepath, onlyprior = 0)
 })
 
-# sampling based on priors and likelihood
-posterior_samples <- lapply(dfs, function(df){
-    fit_model(data, onlyprior = 0)
+# extract median of parameter samples
+param_MPD <- pblapply(samples_list, function(samples){
+    alpha_MPD <- MPD(as_draws_matrix(samples$draws(variables = "alpha")))
+    tau_MPD <- MPD(as_draws_matrix(samples$draws(variables = "tau")))
+    
+    return(list(alpha_MPD, tau_MPD))
 })
 
-# extract choice predictions
-prior_predict_samples <- lapply(prior_samples, function(samples){
-    extract(samples)$choice_pred # dim: 8000 samples x n_trials
-})
+process_for_param_recov_plot <- function(dfs, param_MPD) {
+    param_df <- data.frame()
+    
+    # get the true parameters based on dfs
+    for (i in 1:length(dfs)) {
+        agent_id <- dfs[[i]]$agent_id[1]
+        true_alpha <- dfs[[i]]$alpha[1]
+        true_tau <- dfs[[i]]$tau[1]
+      
+        MPD_alpha <- param_MPD[[i]][[1]]
+        MPD_tau <- param_MPD[[i]][[2]]
 
-posterior_predict_samples <- lapply(posterior_samples, function(samples){
-    extract(samples)$choice_pred
-})
+        param_df <- rbind(param_df, data.frame(agent_id, true_alpha, MPD_alpha, true_tau, MPD_tau))
+    }
+    return (param_df)
+}
 
-# calculate proportion of right-hand choices (rowwise proportion of 1s)
-prior_predict_right <- lapply(prior_predict_samples, function(samples){
-    rowMeans(samples)
-})
+param_df <- process_for_param_recov_plot(dfs, param_MPD)
 
-posterior_predict_right <- lapply(posterior_predict_samples, function(samples){
-    rowMeans(samples)
-})
+# plot MPDs versus true values
+recovery_plot <- function(param_df, parameter){
+    plot <- param_df %>%
+        ggplot(aes(x = !!sym(paste0("MPD_", parameter)), y = !!sym(paste0("true_", parameter)))) + 
+        geom_point() +
+        geom_abline(intercept = 0, slope = 1, color = "red") +
+        labs(title = paste0("True ", parameter, " vs MPD ", parameter),
+             y = paste0("True ", parameter),
+             x = paste0("MPD ", parameter)) +
+        theme_minimal()
 
-# prior predictive check plot from the first agent
-prior_predictive_check <- prior_predict_right[[1]] %>%
-    as_tibble() %>%
-    ggplot(aes(x = value)) +
-    geom_histogram(binwidth = 0.01) +
-    labs(title = "Prior predictive check",
-         x = "Proportion of right-hand choices",
-         y = "Frequency")
+    ggsave(here::here("plots", "recovery", paste0(parameter, "_recovery.jpg")), plot)
+}
 
-ggsave(here::here("plots", "prior_predictive_check.png"), plot = prior_predictive_check)
+for (parameter in c("alpha", "tau")) {
+    recovery_plot(param_df, parameter)
+}
 
-# posterior predictive check plot from the first agent
-posterior_predictive_check <- posterior_predict_right[[1]] %>%
-    as_tibble() %>%
-    ggplot(aes(x = value)) +
-    geom_histogram(binwidth = 0.01) +
-    labs(title = "Posterior predictive check",
-         x = "Proportion of right-hand choices",
-         y = "Frequency")
-
-ggsave(here::here("plots", "posterior_predictive_check.png"), plot = posterior_predictive_check)
