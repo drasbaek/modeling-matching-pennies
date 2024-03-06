e562cd4f0125cf415e9538012965f4e78997782c
MinaAlmasi <mina.almasi00@gmail.com>
2024-03-06 14:17:20 +0100

    rename to run_model, save predictive check data, mv plotting functionality to plot

--- src/predictive_checks/run_model.R
+++ src/predictive_checks/run_model.R
@@ -9,7 +9,7 @@ fit_model <- function(df, onlyprior = 1){
     # prepare the data
     data <- list("trials" = length(df$trial), 
                  "choice"= df$choices, 
-                 "feedback" = df$feedback, 
+                 "feedback" = df$feedback, #ifelse(df$feedback == 0, -1, 1), 
                  "onlyprior" = onlyprior)
     
     # compile the model
@@ -42,7 +42,6 @@ dfs <- split(df, f = df$scenario)
 #' arbitrary choice of fitting in scenario 1, because the model never sees the data anywas when onlyPrior=1
 prior_samples <- fit_model(dfs[[1]], onlyprior = 1)
 
-
 # sampling based on priors and likelihood
 #' creates a list of samples objects, one per agent (4 in total)
 posterior_samples <- lapply(dfs, function(df){
@@ -55,7 +54,8 @@ prior_predict_samples <- list(value1 = as_draws_matrix(prior_samples$draws(varia
 
 posterior_predict_samples <- lapply(posterior_samples, function(samples){
     list(value1 = as_draws_matrix(samples$draws(variables= "value1")), 
-         value2 = as_draws_matrix(samples$draws(variables= "value2"))) 
+         value2 = as_draws_matrix(samples$draws(variables= "value2"))
+         ) 
 })
 
 # prepare data for prior predictive check
@@ -79,16 +79,20 @@ posterior_predict_dfs <- lapply(posterior_predict_samples, function(values_list)
     value2_3rd_quartile <- apply(values_list$value2, 2, quantile, probs = 0.75)
     value1_1st_quartile <- apply(values_list$value1, 2, quantile, probs = 0.25)
     value2_1st_quartile <- apply(values_list$value2, 2, quantile, probs = 0.25)
-    
-    return(data.frame(trial = 1:n_trials, 
+
+    df <- data.frame(trial = 1:n_trials, 
                       value1_median = value1_median, 
                       value2_median = value2_median, 
                       value1_3rd_quartile = value1_3rd_quartile, 
                       value2_3rd_quartile = value2_3rd_quartile, 
                       value1_1st_quartile = value1_1st_quartile, 
-                      value2_1st_quartile = value2_1st_quartile))
-    
+                      value2_1st_quartile = value2_1st_quartile
+                      )
+    return(df)
 })
+
+# check that the names are the same
+names(posterior_predict_dfs) == names(dfs)
 
 # add hider's choice and alpha and tau to the data
 for (i in 1:length(posterior_predict_dfs)){
@@ -98,30 +102,9 @@ for (i in 1:length(posterior_predict_dfs)){
 }
 
 
-# predictive check plot function
-predictive_check_plot <- function(predict_df, title){
-    plot <- ggplot(predict_df, aes(x = trial)) +
-        geom_ribbon(aes(ymin = value1_1st_quartile, ymax = value1_3rd_quartile), fill = "lightblue", alpha = 0.5) +
-        geom_line(aes(y = value1_median), color = "blue") +
-        geom_ribbon(aes(ymin = value2_1st_quartile, ymax = value2_3rd_quartile), fill = "lightgreen", alpha = 0.5) +
-        geom_line(aes(y = value2_median), color = "green") +
-        geom_line(aes(y = hider_choice), color = "red") +
-        labs(title = title, x = "Trial", y = "Value") +
-        theme_bw()
-  
-    # make save_title by formatting title
-    save_title <- gsub(", ", "_", title)
-    save_title <- gsub(" ", "_", save_title)
-    save_title <- gsub("=", "", save_title)
-    ggsave(here::here("plots", paste0(save_title,".jpg")), plot = plot, width = 20, height = 6)
-}
+# save the data
+write_csv(prior_predict_df, here::here("data", "predictive_checks", "prior_predict.csv"))
 
-
-# plot prior predictive check
-predictive_check_plot(prior_predict_df, "Prior Predictive Check")
-
-# plot posterior predictive check
 for (i in 1:length(posterior_predict_dfs)){
-    title <- paste0("Posterior Predictive Check, alpha=", dfs[[i]]$alpha[1], ", tau=", dfs[[i]]$tau[1])
-    predictive_check_plot(posterior_predict_dfs[[i]], title)
+    write_csv(posterior_predict_dfs[[i]], here::here("data", "predictive_checks", paste0("posterior_", names(posterior_predict_dfs)[i], ".csv")))
 }
