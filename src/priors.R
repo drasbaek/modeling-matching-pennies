d6e25c3dc622c682dad8fb6efa09d7a3eaf49802
MinaAlmasi <mina.almasi00@gmail.com>
2024-03-12 11:49:12 +0100

    add tau sigmoid curves

--- src/priors.R
+++ src/priors.R
@@ -1,4 +1,5 @@
 # script for plotting priors
+pacman::p_load(tidyverse)
 
 # plot prior (0, 1)
 logtaus <- rnorm(1000, 0, 1)
@@ -10,25 +11,30 @@ logtaus <- rnorm(1000, 0, 0.2)
 taus <- exp(logtaus)
 hist(taus, breaks=50, main="Prior distribution of tau", xlab="tau", col="lightblue", border="black")
 
-# plot sigmoid curves 
-x_values <- seq(-10, 10, length=100)
-tau_test <- c(0.1, 0.2, 0.3, 0.4, 0.7, 1, 2, 5, 10)
-for (tau in tau_test) {
-  y_values <- c()
-  for (x in x_values) {
-    y <- 1 / (1 + exp(x*-tau))
-    y_values <- c(y_values, y)
+plot_sigmoid_curves <- function(x_values, tau_values, colors, num_to_plot, filename) {
+  plots <- list()
+  
+  for (i in 1:num_to_plot) {
+    tau <- tau_values[i]
+    y_values <- 1 / (1 + exp(-x_values * tau))
+    df <- data.frame(x = x_values, y = y_values)
+    p <- ggplot(df, aes(x, y)) +
+      geom_line(color = colors[i], aes(linewidth = 0.5)) + 
+      labs(title = paste("Sigmoid curve with tau =", tau), x = "x", y = "y") +
+      theme_bw()
+      # remove legend
+      p <- p + theme(legend.position="none")
+    plots[[i]] <- p
   }
-  plot(x_values, y_values, type="l", main=paste("Sigmoid curve with tau=", tau), xlab="x", ylab="y")
+  final_plot <- do.call(grid.arrange, c(plots, ncol = 3, nrow = 2))
+  ggsave(filename, final_plot)
 }
 
-# determining the space of value (set feedback to either 0 or 1)
-n_trials <- 20
-values <- c(0.5)
-alpha <- 0.3
-feedback <- 0
-for (trial in 2:n_trials) {
-  value <- values[trial-1] + alpha * (feedback - values[trial-1])
-  values <- c(values, value)
-}
-plot(1:n_trials, values, type="l", main="Learning curve", xlab="Trial", ylab="Value")
+# Define x_values and tau_values
+x_values <- seq(-10, 10, length=100)
+tau_values <- c(0.1, 0.5, 1, 2, 3, 4) # Example tau values
+colors <- c('#C9D4F6','#c6dbef','#9ecae1','#6baed6','#3182bd','#08519c')
+
+# Plot three values of tau beside each other
+filepath <- file.path("plots", "tau_sigmoid_curves.jpg")
+plot_sigmoid_curves(x_values, tau_values, colors, length(tau_values), filepath)
