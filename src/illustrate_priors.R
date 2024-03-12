bf55c698370cdfd7bc8e8057b53476b3058738e5
MinaAlmasi <mina.almasi00@gmail.com>
2024-03-12 17:31:03 +0100

    add illustrations of prior values

--- src/illustrate_priors.R
+++ src/illustrate_priors.R
@@ -1,7 +1,7 @@
 # script for plotting sigmoid curve +  priors
 pacman::p_load(tidyverse, ggpubr)
 
-plot_sigmoid_curves <- function(x_values, tau_values, colors, n_col, n_rows, filename) {
+plot_sigmoid_curves <- function(x_values, tau_values, colors, n_col, n_rows) {
   plots <- list()
   
   for (i in 1:length(tau_values)) {
@@ -16,8 +16,7 @@ plot_sigmoid_curves <- function(x_values, tau_values, colors, n_col, n_rows, fil
       p <- p + theme(legend.position="none")
     plots[[i]] <- p
   }
-  final_plot <- ggarrange(plotlist = plots, ncol = )
-  ggsave(filename, final_plot)
+  final_plot <- ggarrange(plotlist = plots, ncol = n_col, nrow = n_rows)
 
   return(final_plot)
 }
@@ -28,17 +27,18 @@ tau_values <- c(0.1, 0.5, 1, 2, 3, 4) # Example tau values
 colors <- c('#C9D4F6','#c6dbef','#9ecae1','#6baed6','#3182bd','#08519c')
 
 # plot three values of tau beside each other
-filepath <- file.path("plots", "tau_sigmoid_curves.jpg")
-plot <- plot_sigmoid_curves(x_values, tau_values, colors, n_col = 3, n_rows = 2, filepath)
+filepath <- file.path("plots", "illustrate_priors", "tau_sigmoid_curves.jpg")
+tau_sigmoid_curves <- plot_sigmoid_curves(x_values, tau_values, colors, n_col = 3, n_rows = 2)
+ggsave(filepath, tau_sigmoid_curves, width = 10, height = 6)
 
 plot_tau_priors <- function(mean1, sd1, mean2, sd2) {
   # generate data for the first prior distribution
-  logtaus1 <- rnorm(1000, mean1, sd1)
+  logtaus1 <- rnorm(8000, mean1, sd1)
   taus1 <- exp(logtaus1)
   data1 <- data.frame(taus = taus1, prior = paste("Lognormal (", mean1, ", ", sd1, ")", sep = ""))
   
   # generate data for the second prior distribution
-  logtaus2 <- rnorm(1000, mean2, sd2)
+  logtaus2 <- rnorm(8000, mean2, sd2)
   taus2 <- exp(logtaus2)
   data2 <- data.frame(taus = taus2, prior = paste("Lognormal (", mean2, ", ", sd2, ")", sep = ""))
   
@@ -68,6 +68,42 @@ plot_tau_priors <- function(mean1, sd1, mean2, sd2) {
 }
 
 # plot and save
-plot <- plot_tau_priors(0, 0.2, 0, 1)
-ggsave(file.path("plots", "tau_priors.jpg"), plot, width = 10, height = 6)
+tau_plot <- plot_tau_priors(0, 0.2, 0, 1)
+ggsave(file.path("plots", "illustrate_priors", "tau_priors.jpg"), tau_plot, width = 10, height = 6)
 
+plot_alpha_priors <- function() {
+  # generate data for a uniform prior distribution
+  alphas1 <- runif(8000, 0, 1)
+  data1 <- data.frame(alphas = alphas1, prior = "Uniform (0, 1)")
+
+  # generate data for a beta prior distribution
+  alphas2 <- rbeta(8000, 2, 2)
+  data2 <- data.frame(alphas = alphas2, prior = "Beta (2, 2)")
+  
+  # combine the data
+  combined_data <- rbind(data1, data2)
+  
+  # plot
+  plot <- ggplot(combined_data, aes(x = alphas, fill = prior)) +
+    geom_density(alpha = 0.5) +
+    labs(title = "Prior distribution of alpha",
+         x = "alpha",
+         y = "Density") +
+    xlim(0, 1) +
+    scale_fill_manual(values = c("darkgrey", "#FF0000")) +
+    theme_bw()+
+    theme(legend.position="bottom",
+          legend.title=element_blank(), 
+          legend.key.size = unit(0.4, 'cm'), 
+          legend.text = element_text(size = 10),
+          legend.box.spacing = unit(5, "pt"),
+          axis.text=element_text(size=10), 
+          axis.title=element_text(size=12), 
+          plot.title = element_text(size = 14))
+
+  
+  return(plot)
+}
+
+alpha_plot <- plot_alpha_priors()
+ggsave(file.path("plots", "illustrate_priors", "alpha_priors.jpg"), alpha_plot, width = 10, height = 6)
